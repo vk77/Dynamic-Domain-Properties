01ebb54d8dc347daf4c009850aa58f43237d4081
Robert Fischer <robert@robert-mbp.local>
2010-01-06 22:24:13 -0500

    Looks like adding % in between things are fine

---
+++ CheckEmbeddedPercentInLike/test/integration/FooTests.groovy
@@ -0,0 +1,21 @@
+import grails.test.*
+
+class FooTests extends GrailsUnitTestCase {
+    protected void setUp() {
+      super.setUp()
+      Foo.list()*.delete(flush:true)
+    }
+
+    protected void tearDown() {
+      Foo.list()*.delete(flush:true)
+      super.tearDown()
+    }
+
+    void testFramework() { assertTrue true }
+
+    void testPercentInMiddle() {
+      assert 0 == Foo.count()
+      new Foo(name:"foobar").save(flush:true)
+      assert Foo.findByNameLike("f%bar") != null
+    }
+}
